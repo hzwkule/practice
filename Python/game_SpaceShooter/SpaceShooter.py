792cd41184a1f7702730909166a4f9c20a51a92c
unknown <21450851@student.uwa.edu.au>
2019-09-12 20:03:54 +0800

    day3

--- Python/game_SpaceShooter/SpaceShooter.py
+++ Python/game_SpaceShooter/SpaceShooter.py
@@ -1,23 +1,41 @@
 import pygame
 import random
+from os import path
 
 SCREEN_WIDTH = 480
 SCREEN_HEIGHT = 600
 SCREEN_SIZE = (SCREEN_WIDTH,SCREEN_HEIGHT)
-WHITE = (255,255,255)
-RED = (255,0,0)
-GREEN = (0,255,0)
-BLUE = (0,0,255)
+NEW_ENEMY_GENERATE_INTERVAL = 500
 
 center_x = center_y = 300
 arrow_key_status = [0,0,0,0]
 
+class Explosion(pygame.sprite.Sprite):
+    def __init__(self, center):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(explosion_animation[0], (72,70))
+        self.image.set_colorkey((0,0,0))
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+        self.frame = 0
+        self.last_time = pygame.time.get_ticks()
+
+    def update(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_time > 30:
+            if self.frame < len(explosion_animation):
+                self.image = pygame.transform.scale(explosion_animation[self.frame], (72,70))
+                self.image.set_colorkey((0,0,0))
+                self.frame += 1
+                self.last_time = now
+            else:
+                self.kill()
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self,x,y):
         pygame.sprite.Sprite.__init__(self)
-        self.image =  pygame.Surface((5,10))
-        self.image.fill(BLUE)
+        self.image = bullet_img
+        self.image.set_colorkey((0,0,0))
         self.rect = self.image.get_rect()
         self.rect.centerx = x
         self.rect.centery = y
@@ -28,12 +46,17 @@ class Bullet(pygame.sprite.Sprite):
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((30,30))
-        self.image.fill(RED)
+        img_width = random.randint(20,120)
+        self.image = pygame.transform.scale(enemy_img, (img_width, img_width))
+        self.image.set_colorkey((0,0,0))
         self.rect = self.image.get_rect()
+        self.rect.bottom = 0
+        self.radius = 35
+        #pygame.draw.circle(self.image, (255,0,0), self.rect.center, self.radius)
         self.rect.x = random.randint(0, SCREEN_WIDTH-self.rect.w)
+
         self.vx = random.randint(-2,2)
-        self.vy = random.randint(2,10)
+        self.vy = random.randint(2,6)
         
     def update(self):
         self.rect.x += self.vx
@@ -42,11 +65,13 @@ class Enemy(pygame.sprite.Sprite):
 class Player(pygame.sprite.Sprite):    
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50,50))
-        self.image.fill(GREEN)
+        self.image = pygame.transform.flip(player_img,False,True)
+        self.image = pygame.transform.scale(self.image, (53,40))
+        self.image.set_colorkey((0,0,0))
         self.rect = self.image.get_rect()
         self.rect.centerx = SCREEN_WIDTH/2
         self.rect.bottom = SCREEN_HEIGHT
+        self.radius = 20
         
     def update(self):
         key_state = pygame.key.get_pressed()
@@ -67,13 +92,31 @@ pygame.init()
 screen = pygame.display.set_mode(SCREEN_SIZE)
 pygame.display.set_caption('Space Shooter')
 clock = pygame.time.Clock()
+#now = pygame.time.ticks()
+
+img_dir = path.join(path.dirname(__file__),'img')
+background_dir = path.join(img_dir, 'background.png')
+background_img = pygame.image.load(background_dir).convert()
+background_rect = background_img.get_rect()
+player_dir = path.join(img_dir, 'spaceShips_001.png')
+player_img = pygame.image.load(player_dir).convert()
+enemy_dir = path.join(img_dir, 'spaceMeteors_001.png')
+enemy_img = pygame.image.load(enemy_dir).convert()
+bullet_dir = path.join(img_dir, 'spaceMissiles_027.png')
+bullet_img = pygame.image.load(bullet_dir).convert()
+
+explosion_animation = []
+for i in range(9):
+    explosion_dir = path.join(img_dir, 'regularExplosion0{}.png'.format(i))
+    explosion_img = pygame.image.load(explosion_dir).convert()
+    explosion_animation.append(explosion_img)
 
 player = Player()
 enemies = pygame.sprite.Group()
-for i in range(10):
-    enemy = Enemy()
-    enemies.add(enemy)
 bullets = pygame.sprite.Group()
+explosions = pygame.sprite.Group()
+
+last_enemy_generate_time = 0
 
 game_over = False
 while not game_over:
@@ -110,6 +153,12 @@ while not game_over:
             elif event.key == pygame.K_RIGHT:
                 arrow_key_status[3] = 0
 
+    now = pygame.time.get_ticks()
+    if now - last_enemy_generate_time > NEW_ENEMY_GENERATE_INTERVAL:
+        enemy = Enemy()
+        enemies.add(enemy)
+        last_enemy_generate_time = now
+    
     if arrow_key_status[0]:
         center_y -=1
     if arrow_key_status[1]:
@@ -119,24 +168,31 @@ while not game_over:
     if arrow_key_status[3]:
         center_x +=1
 
-    
-    screen.fill(WHITE)
-
     player.update()
     enemies.update()
     bullets.update()
+    explosions.update()
 
-    hits = pygame.sprite.spritecollide(player, enemies, False)
+    #hits = pygame.sprite.spritecollide(player, enemies, False, pygame.sprtie.collide_rect_ratio(0.7))
+    hits = pygame.sprite.spritecollide(player, enemies, False, pygame.sprite.collide_circle)
     if hits:
         game_over = True
-    if pygame.sprite.groupcollide(enemies, bullets, True, True):
-        for i in range(2):
-            enemy = Enemy()
-            enemies.add(enemy)
-        enemies.update()
+##    if pygame.sprite.groupcollide(enemies, bullets, True, True):
+##        for i in range(2):
+##            enemy = Enemy()
+##            enemies.add(enemy)
+##        enemies.update()
+
+    hits = pygame.sprite.groupcollide(enemies, bullets, True, True)
+    for hit in hits:
+        explosion = Explosion(hit.rect.center)
+        explosions.add(explosion)
+    
+    screen.blit(background_img,background_rect)
     screen.blit(player.image,player.rect)
     enemies.draw(screen)
     bullets.draw(screen)
+    explosions.draw(screen)
     
     pygame.display.flip()
 
